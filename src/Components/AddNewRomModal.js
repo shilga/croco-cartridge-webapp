e884396fdc1271e1d733651d7304da533f02bf7f
Sebastian Quilitz <zeraphim@x-pantion.de>
2025-01-13 21:45:40 +0100

    Prompt a proper error messaage if ROM name is empty

--- src/Components/AddNewRomModal.js
+++ src/Components/AddNewRomModal.js
@@ -27,8 +27,7 @@ const BANK_SIZE = 0x4000;
 const CHUNK_SIZE = 32;
 const CHUNKS_PER_BANK = BANK_SIZE / CHUNK_SIZE;
 
-class AddNewRomModal extends React.Component
-{
+class AddNewRomModal extends React.Component {
     show = this.props.show;
     onHide = this.props.onHide;
     onRomAdded = this.props.onRomAdded;
@@ -45,9 +44,8 @@ class AddNewRomModal extends React.Component
 
     rom;
 
-    onEnterHandler()
-    {
-        this.setState({validRomLoaded: false});
+    onEnterHandler() {
+        this.setState({ validRomLoaded: false });
     }
 
     fileChangedHandler(e) {
@@ -55,7 +53,7 @@ class AddNewRomModal extends React.Component
         const fileObj = e.target.files && e.target.files[0];
         if (!fileObj) {
             console.log("no file selected");
-            this.setState({validRomLoaded: false});
+            this.setState({ validRomLoaded: false });
             return;
         }
         const enc = new TextDecoder("utf-8");
@@ -66,52 +64,42 @@ class AddNewRomModal extends React.Component
             var banks = 1 << (this.rom[0x0148] + 1);
             var nameArray = this.rom.subarray(0x134, 0x134 + 16);
             var isCgbGame = this.rom[0x143] === 0xC0 || (this.rom[0x143] === 0x80);
-            var zero = nameArray.findIndex((element, index, array) => {return element === 0;})
-            if(zero === -1)
-            {
+            var zero = nameArray.findIndex((element, index, array) => { return element === 0; })
+            if (zero === -1) {
                 zero = isCgbGame ? 15 : 16;
             }
 
             var name = enc.decode(this.rom.subarray(0x134, 0x134 + zero));
-            
+
             console.log("ROM has " + banks + " banks");
             console.log("ROM name is " + name);
             console.log("ROM is a CGB game: " + isCgbGame);
 
             var speedchangeBank = 0xffff;
-            if(isCgbGame)
-            {
+            if (isCgbGame) {
                 var speedchangeState = 0;
-                for(var i=0; (i<this.rom.byteLength) && (speedchangeBank === 0xffff); i++)
-                {
+                for (var i = 0; (i < this.rom.byteLength) && (speedchangeBank === 0xffff); i++) {
                     var inst = this.rom[i];
-                    if(speedchangeState === 0)
-                    {
-                        if(inst === 0xe0)
-                        {
+                    if (speedchangeState === 0) {
+                        if (inst === 0xe0) {
                             speedchangeState = 1;
                         }
                     }
-                    else if(speedchangeState === 1)
-                    {
-                        if(inst === 0x4d)
-                        {
+                    else if (speedchangeState === 1) {
+                        if (inst === 0x4d) {
                             speedchangeState = 2;
                         }
-                        else
-                        {
+                        else {
                             speedchangeState = 0;
                         }
                     }
-                    else if(speedchangeState === 2)
-                        {
-                        if(inst === 0x10)
-                        {
+                    else if (speedchangeState === 2) {
+                        if (inst === 0x10) {
                             speedchangeState = 4;
                             speedchangeBank = Math.floor(i / BANK_SIZE);
                             console.log("Speed change stop at " + i.toString(16));
                         }
-                        else if(inst === 0xc9) // ret
+                        else if (inst === 0xc9) // ret
                         {
                             speedchangeState = 0;
                         }
@@ -119,57 +107,58 @@ class AddNewRomModal extends React.Component
                         {
                             speedchangeState = 0;
                         }
-                        else if( inst === 0xe0)
-                        {
+                        else if (inst === 0xe0) {
                             speedchangeState = 3;
                         }
                     }
-                    else if(speedchangeState === 3)
-                    {
+                    else if (speedchangeState === 3) {
                         // just ignore this byte and go back to wait for stop state
                         speedchangeState = 2;
                     }
-                    else
-                    {
+                    else {
                         console.log("That should not have happened");
                     }
                 }
             }
 
             console.log("Speed change happens in bank " + speedchangeBank);
-            this.setState({validRomLoaded: true, romInfo: { banks: banks, name: name, speedchangeBank: speedchangeBank } });
+            this.setState({ validRomLoaded: true, romInfo: { banks: banks, name: name, speedchangeBank: speedchangeBank } });
         };
         reader.readAsArrayBuffer(fileObj);
     }
 
-    async romUploadButtonHandler () {
-        console.log("Starting upload of " + this.state.romInfo.banks + " banks with speedSwitchBank=" + this.state.romInfo.speedchangeBank);
-
-        this.setState({uploadInProgress: true, uploadRequestInProgress: true, uploadedBank: this.state.romInfo.banks});
-        try {
-        await this.comm.requestRomUploadCommand(this.state.romInfo.banks, this.state.romInfo.name, this.state.romInfo.speedchangeBank);
-
-
-        console.log("Upload was accepted");
-
-        this.setState({uploadRequestInProgress: false, uploadedBank: 0});
-
-        for (var bank = 0; bank < this.state.romInfo.banks; bank++) {
-            for (var chunk = 0; chunk < CHUNKS_PER_BANK; chunk++) {
-                await this.comm.sendRomChunkCommand(bank, chunk, this.rom.subarray((bank * BANK_SIZE) + (chunk * CHUNK_SIZE), (bank * BANK_SIZE) + ((chunk + 1) * CHUNK_SIZE)));
-                console.log("Bank " + bank + " chunk " + chunk);
-            }
-            this.setState({uploadedBank: bank});
+    async romUploadButtonHandler() {
+        if(this.state.romInfo.name === "") {
+            this.onError("ROM name must not be empty!");
+            return;
         }
 
-        console.log("Upload finished");
-        this.setState({uploadInProgress: false});
+        console.log("Starting upload of " + this.state.romInfo.banks + " banks with speedSwitchBank=" + this.state.romInfo.speedchangeBank);
 
-        this.onRomAdded();
-        } catch (e)
-        {
+        this.setState({ uploadInProgress: true, uploadRequestInProgress: true, uploadedBank: this.state.romInfo.banks });
+        try {
+            await this.comm.requestRomUploadCommand(this.state.romInfo.banks, this.state.romInfo.name, this.state.romInfo.speedchangeBank);
+
+
+            console.log("Upload was accepted");
+
+            this.setState({ uploadRequestInProgress: false, uploadedBank: 0 });
+
+            for (var bank = 0; bank < this.state.romInfo.banks; bank++) {
+                for (var chunk = 0; chunk < CHUNKS_PER_BANK; chunk++) {
+                    await this.comm.sendRomChunkCommand(bank, chunk, this.rom.subarray((bank * BANK_SIZE) + (chunk * CHUNK_SIZE), (bank * BANK_SIZE) + ((chunk + 1) * CHUNK_SIZE)));
+                    console.log("Bank " + bank + " chunk " + chunk);
+                }
+                this.setState({ uploadedBank: bank });
+            }
+
+            console.log("Upload finished");
+            this.setState({ uploadInProgress: false });
+
+            this.onRomAdded();
+        } catch (e) {
             this.onError("Uploading the ROM failed");
-            this.setState({uploadInProgress: false});
+            this.setState({ uploadInProgress: false });
         }
     }
 
@@ -189,19 +178,19 @@ class AddNewRomModal extends React.Component
                         <Form.Group as={Row}>
                             <Form.Label column sm="2">Select ROM</Form.Label>
                             <Col sm="10">
-                            <Form.Control type="file" onChange={(e) => this.fileChangedHandler(e)} disabled={this.state.uploadInProgress} />
+                                <Form.Control type="file" onChange={(e) => this.fileChangedHandler(e)} disabled={this.state.uploadInProgress} />
                             </Col>
                         </Form.Group>
                         <Form.Group as={Row}>
                             <Form.Label column sm="2">ROM Name:</Form.Label>
                             <Col sm="10">
-                            <Form.Control type="text" maxLength={16} value={this.state.romInfo.name} onChange={(e) => this.setState({romInfo: {name: e.target.value, banks: this.state.romInfo.banks, speedchangeBank: this.state.romInfo.speedchangeBank}})} />
+                                <Form.Control type="text" maxLength={16} value={this.state.romInfo.name} onChange={(e) => this.setState({ romInfo: { name: e.target.value, banks: this.state.romInfo.banks, speedchangeBank: this.state.romInfo.speedchangeBank } })} />
                             </Col>
                         </Form.Group>
                         <Form.Group as={Row}>
                             <Form.Label column sm="2">ROM Banks:</Form.Label>
                             <Col sm="10">
-                            <Form.Control plaintext readOnly value={this.state.romInfo.banks} />
+                                <Form.Control plaintext readOnly value={this.state.romInfo.banks} />
                             </Col>
                         </Form.Group>
                     </Form>
